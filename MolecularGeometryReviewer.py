1605716ff300fd6233055cd0ad52ad8bb7b00250
RazeenHossain <47703658+RazeenHossain@users.noreply.github.com>
2020-07-09 22:14:05 -0400

    Prompt and fixed answer checking
    Previous  version  never actually checked if user input was correct.
    
    Now prompts user with question.

--- MolecularGeometryReviewer.py
+++ MolecularGeometryReviewer.py
@@ -2,16 +2,17 @@ import random
 import pandas as pd
 
 df = pd.read_csv('Datatable.csv')
-
+print(
+    "Enter the correct molecular geometry based on the number of bonding groups and lone pairs of electrons given. "
+    "Enter exit to quit.")
 while True:
     randNum = random.randrange(0, 12)
     answer = df['molecularGeometry'][randNum]
-    print(df['bonds'][randNum], df['lonePairs'][randNum])
+    print("Bonding Domains: ", df['bonds'][randNum], "\nLone Pairs: ", df['lonePairs'][randNum])
     guess = input()
-    if answer == guess:
-        print("Correct")
-        if guess == "exit":
-            break
-
-        else:
-            print("Incorrect")
+    if answer == guess.casefold():
+        print("Correct\n")
+    elif guess == "exit":
+        break
+    else:
+        print("Incorrect\n")
